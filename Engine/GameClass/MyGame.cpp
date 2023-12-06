6f2db86481d9e21fc6c06a04a000d6f89015bf32
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2023-12-06 16:28:24 +0900

    MyGame内を掃除

--- Engine/GameClass/MyGame.cpp
+++ Engine/GameClass/MyGame.cpp
@@ -33,16 +33,9 @@ void MyGame::Update()
 	//	ゲーム内の処理		//
 	//------------------*/
 
-	// 任意軸回転
-	Quaternion q1 = { 2.0f,3.0f,4.0f,1.0f };
-	Quaternion q2 = { 1.0f,3.0f,5.0f,2.0f };
-	Quaternion identity = Quaternion::IdentityQuaternion();
-	Quaternion conj = Quaternion::Conjugate(q1);
-	Quaternion inv = Quaternion::Inverse(q1);
-	Quaternion normal = Quaternion::Normalize(q1);
-	Quaternion mul1 = Quaternion::Multiply(q1, q2);
-	Quaternion mul2 = Quaternion::Multiply(q2, q1);
-	float norm = Quaternion::Norm(q1);
+
+
+
 
 	/*////////////
 	//	ImGui	//
@@ -51,14 +44,6 @@ void MyGame::Update()
 #ifdef _DEBUG
 
 	ImGui::Begin("DebugConsole");
-
-	ImGui::Text("Identity     : %.2f,%.2f,%.2f,%.2f", identity.x, identity.y, identity.z,identity.w);
-	ImGui::Text("Conjugate    : %.2f,%.2f,%.2f,%.2f", conj.x, conj.y, conj.z, conj.w);
-	ImGui::Text("Inverse      : %.2f,%.2f,%.2f,%.2f", inv.x, inv.y, inv.z, inv.w);
-	ImGui::Text("Normalize    : %.2f,%.2f,%.2f,%.2f", normal.x, normal.y, normal.z, normal.w);
-	ImGui::Text("Mul(q1,q2)   : %.2f,%.2f,%.2f,%.2f", mul1.x, mul1.y, mul1.z, mul1.w);
-	ImGui::Text("Mul(q2,q1)   : %.2f,%.2f,%.2f,%.2f", mul2.x, mul2.y, mul2.z, mul2.w);
-	ImGui::Text("Norm         : %.2f", norm);
 
 	ImGui::End();
 
